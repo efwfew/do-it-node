39547be377ab3317f10b1aa6a26dcf6ef9d203f6
PeterKimG <myopinion@naver.com>
2020-07-12 22:10:42 +0900

    the author is fucking idiot

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var serveStatic = require('serve-static');
 var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
+var router = express.Router();
+var expressErrorHandler = require('express-error-handler');
+var cookieParser = require('cookie-parser');
 
 //기본 포트를 app 객체에 속성으로 설정
 //process.env 객체에 PORT 속성이 있으면 그걸 사용, 없으면 3000
@@ -16,11 +19,17 @@ app.use(bodyParser.urlencoded({extended: false}));
 // body-parser를 사용해 application/json 파싱
 app.use(bodyParser.json());
 
-app.use(serveStatic(path.join(__dirname, 'public')));
+// cookie-parser 사용
+app.use(cookieParser);
+
+app.use('/public', serveStatic(path.join(__dirname, 'public')));
 
 // middle-ware
-app.use(function(req, res, next){
-    console.log('first middle-ware handled request');
+router.route('/process/login/:name').post(function(req, res) {
+    console.log('/process/login/:name 처리');
+
+    var paramName = req.params.name;
+
     var paramId = req.body.id || req.query.id;
     var paramPassword = req.body.password || req. query.password;
     // var userAgent = req.header('User-Agent');
@@ -29,11 +38,29 @@ app.use(function(req, res, next){
 
     res.writeHead('200', {'Content-Type':'text/html;charset=utf-8'});
     res.write('<h1>Express server responding</h1>');
-    res.write('<div><p>User-Agent : ' + paramId + ' </p></div>')
-    res.write('<div><p>ParamName : ' + paramPassword + ' </p></div>')
+    res.write('<div><p>Param name : ' + paramName + ' </p></div>')
+    res.write('<div><p>Param id : ' + paramId + ' </p></div>')
+    res.write('<div><p>Param password : ' + paramPassword + ' </p></div>')
+    res.write("<br><br><a href='/public/login.html'>로그인 페이지로 돌아가기</a>")
     res.end();
 })
 
+app.use('/', router);
+
+//등록되지 않은 패스에 대해 페이지 오류 응답
+// app.all('*', function(req, res){
+//     res.status(404).send('<h1>ERROR - 페이지를 찾을수 없습니다.</h1>')
+// });
+
+// 404 에러 페이지 처리
+var errorHandler = expressErrorHandler({
+    static: {
+      '404': './public/404.html'
+    }
+});
+
+app.use( expressErrorHandler.httpError(404) );
+app.use( errorHandler );
 
 
 //express 서버 시작
